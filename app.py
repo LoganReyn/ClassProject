c9bd951a220790e21f86a2b044f2b0f875dcfe7a
Calancaster <gudboidindunuffin@gmail.com>
2023-11-30 16:36:34 -0500

    Implementing Chat API

--- app.py
+++ app.py
@@ -17,20 +17,31 @@ def index():
         response = requests.get(url)
         soup = BeautifulSoup(response.text, 'html.parser')
         tags = ['div', 'p']
-        summary = ''
+        articleText = helpers.getSoup(url)
 
-        for tag in soup.findAll('p'):
-            print(tag)
-            if tag.name not in tags:
-                tag.replaceWith(tag.renderContents())
-            else:
-                summary = summary + tag.getText()
-
-        return redirect(url_for('summary', summary=summary, title='title'))
+        return redirect(url_for('summary', articleText=articleText))
     
     elif request.method == 'GET':
         return render_template('index.html')
     
-@app.route('/summary/<title>')
-def summary(summary, title):
+@app.route('/summary/<articleText>')
+def summary(articleText):
+
+    chatPrompt = f'''Goal:
+
+    Assign a content rating to the provided article using the following scale:
+
+    E: Suitable for Everyone
+    T: Appropriate for Teens
+    M: Intended for a Mature audience
+    Summarize the key points of the article in no more than two paragraphs.
+
+    Example Output Format:
+
+    Maturity Rating: [Insert E, T, or M based on content assessment]
+    Article Summary: [Provide a concise and informative summary, covering the main points of the article in up to two paragraphs.]
+    Article for Reference:
+    <<{articleText}>>'''
+    summary = helpers.llm_response(chatPrompt)
+
     return render_template('summary.html', summary=summary)
